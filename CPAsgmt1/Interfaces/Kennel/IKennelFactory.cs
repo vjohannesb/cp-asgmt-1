cf0ad7c19eaf8d5985658911475084dc1ae7857b
vjohannesb <johannes.b.nilsson@gmail.com>
2022-01-06 12:40:45 +0100

    Separate servicefactory from kennelfactory

--- CPAsgmt1/Interfaces/Kennel/IKennelFactory.cs
+++ CPAsgmt1/Interfaces/Kennel/IKennelFactory.cs
@@ -3,7 +3,5 @@
     internal interface IKennelFactory
     {
         IKennel CreateKennel(string name, decimal price);
-
-        IService CreateService(string name, decimal price);
     }
 }
---
+++ CPAsgmt1/Interfaces/Kennel/IServiceFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPAsgmt1.Interfaces.Kennel
+{
+    internal interface IServiceFactory
+    {
+        IService CreateService(string name, decimal price);
+    }
+}
